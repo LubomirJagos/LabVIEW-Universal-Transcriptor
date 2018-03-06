4ed46bab7d10adcdff51d79370bb6f05db32376b
Lubomir Jagos <lubomir.jagos@gmail.com>
2018-03-06 01:28:34 +0100

    Added function to parse ClusterConstant. I put there example file Transcriptor LabVIEW to C code 2.vi which add into generated code definition of Cluster, also added Cluster to C code VIs.
    I worked on how to pass Cluster through wires, and in the end Cluster is just struct, but we somehow have to know how to get item if using Unbundle and Unbundle by name, this is solved following:
      + all Cluster items have names, if not transcriptor deal with it using "LabVIEW Cluster Item Not Named Prefix.vi" with it offset number, that's how it will write into typedef struct, so it doesn't matter what VI (Unbundle or Unbundle By Name) we use, because always we know how to figure out name of item
      - we have to store somewhere in array all Cluster typedef and when it's used and it's not typedef (in case of typedef we can take just typedef name and use it) we have to assign it some name and when wire is Cluster type and it's possible to convert it to some Cluster in memory which have the same datatypes in the same order inside we can declare it like "myClusterInMemoryName wired_UID_245;" (this functionality have to be done)
      - there could be Clusters inside Cluster
      - recursively need to get definitions of all Cluster (going down through all of them)
    
    Anyway example "Test Transcriptor LabVIEW to C code 2.vi" is functional.

---
+++ C code/Test_Cluster/Test_Cluster.ino
@@ -0,0 +1,28 @@
+typedef struct myDefinedCluster{
+  int item0;
+  String item1;
+  double item2[5];
+};
+
+typedef struct myDefinedClusterSimilar{
+  int itemXYZ = 50;
+  String anotherString;
+  double itemZ[5] = {1,2.2,35,4,7};
+};
+
+myDefinedCluster myVariable;
+myDefinedCluster *myVariableRef;
+myDefinedClusterSimilar myVar2;
+myDefinedClusterSimilar *myVar2Ref;
+
+void setup() {
+  // put your setup code here, to run once:
+  myVariable.item0 = 1042;  
+  myVariableRef = new myDefinedCluster();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  myVariable.item0--;
+  myVar2 = *((myDefinedClusterSimilar*) myVariableRef);
+}
