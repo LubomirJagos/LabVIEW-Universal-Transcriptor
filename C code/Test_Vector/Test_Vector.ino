eed5ffd8eb4dedf518ef5c5b00d3efb7fad166a6
Lubomir Jagos <lubomir.jagos@gmail.com>
2018-03-20 02:37:00 +0100

    Added Cluster support, need to check if it's correct. Now it's used for wires datatypes and everywhere, so if there is Cluster which is not typedefinition, it traversed and transcripted into code. Even assign values should be running but need to solve adding quotes to String.
    As I wrote, I put there transcripting Cluster for wire datatypes and other.
    Added Array datatype to "Get Variant Data Type.vi", return as "Array<String><String>", number of brackets specify dimensions count.
    
    UserDefinedWaveform.lvclass is just to test how it is define new datatype as class.

---
+++ C code/Test_Vector/Test_Vector.ino
@@ -0,0 +1,11 @@
+#include <vector>
+
+void setup() {
+  // put your setup code here, to run once:
+  std::vector<int> myVector;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
