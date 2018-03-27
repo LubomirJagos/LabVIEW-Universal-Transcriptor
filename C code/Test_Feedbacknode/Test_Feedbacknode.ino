3595d59f1243cee2031e2477e28aee0929ef97b2
Lubomir Jagos <lubomir.jagos@gmail.com>
2018-03-27 03:43:59 +0200

    Added functional feedbaNode implementaion using CircularBuffer from github. Changed directory structure.
    Because I changed dir structure I have problem that in translator for system VI is path as string so I had to rewrite it.
    I used for feedback node circular registers. There is trick in generating feedback nodes. I had to generate their declaration before structure in which they are, after that I had to define bool value to create single pass case with init values (so var = true, after run case is changed inside to false so it's not running again) and assignements into feedback node has to be at end of diagram declaration, because writing new value into feedback is happening in the end.

--- C code/Test_Feedbacknode/Test_Feedbacknode.ino
+++ C code/Test_Feedbacknode/Test_Feedbacknode.ino
@@ -1,87 +1,180 @@
 #include <StandardCplusplus.h>
 #include <vector>
 #include <ArduinoBasicFunctions.h>
+#include <CircularBuffer.h>
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+#include <empty.h>
+SoftwareSerial* ArduinoLabVIEWcreatedUART_3;
+
+#include <empty.h>
+
+
+#include <ArduinoBasicFunctions.h>
 using namespace std;
 
-#define FEEDBACK_REGISTER_SIZE_417 8
-template<typename FEEDBACKNODE_TYPE> static FEEDBACKNODE_TYPE feedbackNode_417(FEEDBACKNODE_TYPE value){
-    static FEEDBACKNODE_TYPE internalBuffer[FEEDBACK_REGISTER_SIZE_417];
-    static int internalHead = 0;
-    static int internalTail = FEEDBACK_REGISTER_SIZE_417 - 1;
-    internalBuffer[internalHead] = value;
-    internalHead += 1;
-    internalTail += 1;
-    if (internalHead == FEEDBACK_REGISTER_SIZE_417) internalHead = 0;
-    if (internalTail == FEEDBACK_REGISTER_SIZE_417) internalTail = 0;
-    return internalBuffer[internalHead];
-}
-
-#define FEEDBACK_REGISTER_SIZE_36 2
-template<typename FEEDBACKNODE_TYPE> static FEEDBACKNODE_TYPE feedbackNode_36(FEEDBACKNODE_TYPE value){
-    static FEEDBACKNODE_TYPE internalBuffer[FEEDBACK_REGISTER_SIZE_36];
-    static int internalHead = 0;
-    static int internalTail = FEEDBACK_REGISTER_SIZE_36 - 1;
-    internalBuffer[internalHead] = value;
-    internalHead += 1;
-    internalTail += 1;
-    if (internalHead == FEEDBACK_REGISTER_SIZE_36) internalHead = 0;
-    if (internalTail == FEEDBACK_REGISTER_SIZE_36) internalTail = 0;
-    return internalBuffer[internalHead];
-}
-
-
-void Example_Feedbacknode_1(){
+void Blink_5(){
+    unsigned int wireUID_1641_ = 9600;
+    unsigned char wireUID_1634_ = 0;
+    unsigned char wireUID_1629_ = 1;
+    bool wireUID_1576_ = false;
+    long wireUID_1507_ = 3;
+    long wireUID_1396_ = 2;
+    bool wireUID_740_ = true;
+    long wireUID_705_ = 3;
+    long wireUID_652_ = 2;
+    ArduinoLabVIEWcreatedUART_3 = new SoftwareSerial( wireUID_1634_, wireUID_1629_);
     
+    ArduinoLabVIEWcreatedUART_3->begin(wireUID_1641_);
+    
+    pinMode(wireUID_652_,wireUID_740_?OUTPUT:INPUT);
+    pinMode(wireUID_705_,wireUID_740_?OUTPUT:INPUT);
+    digitalWrite(wireUID_1396_,wireUID_1576_?HIGH:LOW);
+    digitalWrite(wireUID_1507_,wireUID_1576_?HIGH:LOW);
     /***** BEGIN WhileLoop ********/
-    bool wireUID_962_; //LoopEndRef.Wire
+    //starts with LoopEndRef.
+    bool wireUID_1608_ = false;
     do{
-        float wireUID_1095_;
-        String wireUID_289_;
-        String wireUID_232_;
-        String wireUID_202_;
-        float wireUID_1207_;
-        float wireUID_1147_ = -4.200000;
-        float wireUID_1071_ = 0.000000;
-        double wireUID_806_ = 4.700000;
-        double wireUID_781_ = -2.200000;
-        bool wireUID_729_;
-        wireUID_729_ = wireUID_806_ > wireUID_781_;
-        wireUID_1207_ = feedbackNode_417(wireUID_1095_);
-        wireUID_232_ = feedbackNode_36(wireUID_202_);
-        /***** BEGIN CaseStructure ********/
+        long wireUID_1394_ = 7;
+        long wireUID_672_ = 7;
+        /**** BEGIN ForLoop *****/
+        CircularBuffer<bool,2> feedbackNode_615;
+        bool feedbackNodeInitBool_615 = true;
         
-        String wireUID_249_ = wireUID_232_;
-        float wireUID_1256_ = wireUID_1207_;
         
-        String wireUID_238_ = wireUID_232_;
-        float wireUID_1039_ = wireUID_1071_; //wires declarations
-        switch(wireUID_729_){
-            case false:
-                
-                 /* C code */
-                wireUID_1095_ = wireUID_1256_;
-                wireUID_202_ = wireUID_249_; /* output assgn. */
-            break;
-            case true:
-                
-                 /* C code */
-                wireUID_1095_ = wireUID_1039_;
-                wireUID_202_ = wireUID_238_; /* output assgn. */
-            break;
+        for (int LVHardcodedCnt = 0;  LVHardcodedCnt < wireUID_672_; LVHardcodedCnt++){
+            bool wireUID_1415_ = true;
+            bool wireUID_642_;
+            bool wireUID_446_;
+            long wireUID_426_ = 1000;
+            delay(wireUID_426_); /* by Translator.vi, VI Path: C:\Users\lubo\Documents\LabView\LabVIEW-Universal-Transcriptor\Test\Examples For Parsing\delay.vi */
+            if (feedbackNodeInitBool_615){
+                feedbackNode_615.push(wireUID_1415_);
+                feedbackNodeInitBool_615 = false;
+            }
+            wireUID_642_ = feedbackNode_615.shift();
+            /***** BEGIN CaseStructure ********/
+            String wireUID_2308_ = "A0";
+            bool wireUID_721_ = true;
+            bool wireUID_618_;
+            long wireUID_606_ = 2;
+            long wireUID_1038_ = 3;
+            String wireUID_2208_ = "A1";
+            bool wireUID_1239_;
+            bool wireUID_760_ = false;
+            long wireUID_1157_ = 2;
+            long wireUID_921_ = 3; //wires declarations
+            switch(wireUID_642_){
+                case false:
+                    
+                    wireUID_618_ = !wireUID_721_;
+                    digitalWrite(wireUID_1038_,wireUID_721_?HIGH:LOW);
+                    digitalWrite(wireUID_606_,wireUID_618_?HIGH:LOW);
+                    ArduinoLabVIEWcreatedUART_3->print(wireUID_2308_);
+                    
+                    
+                    
+                    
+                     /* C code */
+                    wireUID_446_ = wireUID_721_; /* output assgn. */
+                break;
+                case true:
+                    
+                    wireUID_1239_ = !wireUID_760_;
+                    digitalWrite(wireUID_921_,wireUID_760_?HIGH:LOW);
+                    digitalWrite(wireUID_1157_,wireUID_1239_?HIGH:LOW);
+                    ArduinoLabVIEWcreatedUART_3->print(wireUID_2208_);
+                    
+                    
+                    
+                    
+                     /* C code */
+                    wireUID_446_ = wireUID_760_; /* output assgn. */
+                break;
+            }
+            /****** END CaseStructure ********/
+            /* Don't know how to translate class "Generic ->GObject ->Node ->":  */
+            
+            feedbackNode_615.push(wireUID_446_);
+            
+             /* C code*/
+         /* output assignements */
         }
-        /****** END CaseStructure
-        /* Don't know how to translate class "Generic ->GObject ->Node ->":  */
-        wireUID_962_ = (wireUID_1095_ > 0);
-        /* Don't know how to translate class "Generic ->GObject ->Node ->":  */ /* C code */
+        /***** END ForLoop ******/
+        /**** BEGIN ForLoop *****/
+        CircularBuffer<bool,2> feedbackNode_1321;
+        bool feedbackNodeInitBool_1321 = true;
+        
+        
+        for (int LVHardcodedCnt = 0;  LVHardcodedCnt < wireUID_1394_; LVHardcodedCnt++){
+            bool wireUID_1336_ = true;
+            bool wireUID_1339_;
+            bool wireUID_1342_;
+            long wireUID_1349_ = 1000;
+            if (feedbackNodeInitBool_1321){
+                feedbackNode_1321.push(wireUID_1336_);
+                feedbackNodeInitBool_1321 = false;
+            }
+            wireUID_1339_ = feedbackNode_1321.shift();
+            delay(wireUID_1349_); /* by Translator.vi, VI Path: C:\Users\lubo\Documents\LabView\LabVIEW-Universal-Transcriptor\Test\Examples For Parsing\delay.vi */
+            /***** BEGIN CaseStructure ********/
+            String wireUID_3039_ = "B0";
+            bool wireUID_1121_ = true;
+            long wireUID_1140_ = 2;
+            long wireUID_1156_ = 3;
+            String wireUID_2896_ = "B1";
+            bool wireUID_1298_ = false;
+            long wireUID_1308_ = 2;
+            long wireUID_1312_ = 3; //wires declarations
+            switch(wireUID_1339_){
+                case false:
+                    
+                    digitalWrite(wireUID_1156_,wireUID_1121_?HIGH:LOW);
+                    digitalWrite(wireUID_1140_,wireUID_1121_?HIGH:LOW);
+                    ArduinoLabVIEWcreatedUART_3->print(wireUID_3039_);
+                    
+                    
+                    
+                     /* C code */
+                    wireUID_1342_ = wireUID_1121_; /* output assgn. */
+                break;
+                case true:
+                    
+                    digitalWrite(wireUID_1312_,wireUID_1298_?HIGH:LOW);
+                    digitalWrite(wireUID_1308_,wireUID_1298_?HIGH:LOW);
+                    ArduinoLabVIEWcreatedUART_3->print(wireUID_2896_);
+                    
+                    
+                    
+                     /* C code */
+                    wireUID_1342_ = wireUID_1298_; /* output assgn. */
+                break;
+            }
+            /****** END CaseStructure ********/
+            /* Don't know how to translate class "Generic ->GObject ->Node ->":  */
+            feedbackNode_1321.push(wireUID_1342_);
+            
+            
+             /* C code*/
+         /* output assignements */
+        }
+        /***** END ForLoop ******/
+        
+         /* C code */
      /* output assgn. */
-    }while(wireUID_962_);
+    }while(!wireUID_1608_); //negation because in LabVIEW it is when stop (oposite)
     /****** END WhileLoop */
+    
+    
+    
+    
+    
     
     
 }
 
 void setup(){}
 void loop(){
-    Example_Feedbacknode_1();
+    Blink_5();
 }
 
