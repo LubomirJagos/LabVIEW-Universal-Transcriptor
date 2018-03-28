61ae41696104878a822c8ef47a1c62fd807ef033
Lubomir Jagos <ljagos@unis.cz>
2018-03-28 11:24:36 +0200

    Corrected small bugs, wrong input replacing for system VI in Translator.vi, better  generating autogenerated functions input params in prototype.
    Now input params for SubVI auto generated function are created using Terminal, because for this operation transcriptor creates SubVI on block diagram and read its terminals and based on them creates input params list, because when transcriptor creates programmaticaly VI it has not connected any terminal so I cannot read terminal datatypes based on connected wires.

--- C code/Test_Feedbacknode/Test_Feedbacknode.ino
+++ C code/Test_Feedbacknode/Test_Feedbacknode.ino
@@ -2,179 +2,62 @@
 #include <vector>
 #include <ArduinoBasicFunctions.h>
 #include <CircularBuffer.h>
-#include <Arduino.h>
-#include <SoftwareSerial.h>
-#include <empty.h>
-SoftwareSerial* ArduinoLabVIEWcreatedUART_3;
-
-#include <empty.h>
-
-
-#include <ArduinoBasicFunctions.h>
 using namespace std;
 
-void Blink_5(){
-    unsigned int wireUID_1641_ = 9600;
-    unsigned char wireUID_1634_ = 0;
-    unsigned char wireUID_1629_ = 1;
-    bool wireUID_1576_ = false;
-    long wireUID_1507_ = 3;
-    long wireUID_1396_ = 2;
-    bool wireUID_740_ = true;
-    long wireUID_705_ = 3;
-    long wireUID_652_ = 2;
-    ArduinoLabVIEWcreatedUART_3 = new SoftwareSerial( wireUID_1634_, wireUID_1629_);
-    
-    ArduinoLabVIEWcreatedUART_3->begin(wireUID_1641_);
-    
-    pinMode(wireUID_652_,wireUID_740_?OUTPUT:INPUT);
-    pinMode(wireUID_705_,wireUID_740_?OUTPUT:INPUT);
-    digitalWrite(wireUID_1396_,wireUID_1576_?HIGH:LOW);
-    digitalWrite(wireUID_1507_,wireUID_1576_?HIGH:LOW);
+CircularBuffer<double,1> shiftRegister_1173;
+bool shiftRegisterInitBool_1 = true;
+
+CircularBuffer<String,1> shiftRegister_1326;
+
+
+void Example_Shift_Register_Loop_1(double numberA, String userString1){
+    double wireUID_1011_ = numberA;
+    String wireUID_1506_;
+    double wireUID_1464_;
+    String wireUID_1031_ = userString1;
     /***** BEGIN WhileLoop ********/
     //starts with LoopEndRef.
-    bool wireUID_1608_ = false;
+    bool wireUID_1433_ = false;
     do{
-        long wireUID_1394_ = 7;
-        long wireUID_672_ = 7;
-        /**** BEGIN ForLoop *****/
-        CircularBuffer<bool,2> feedbackNode_615;
-        bool feedbackNodeInitBool_615 = true;
-        
-        
-        for (int LVHardcodedCnt = 0;  LVHardcodedCnt < wireUID_672_; LVHardcodedCnt++){
-            bool wireUID_1415_ = true;
-            bool wireUID_642_;
-            bool wireUID_446_;
-            long wireUID_426_ = 1000;
-            delay(wireUID_426_); /* by Translator.vi, VI Path: C:\Users\lubo\Documents\LabView\LabVIEW-Universal-Transcriptor\Test\Examples For Parsing\delay.vi */
-            if (feedbackNodeInitBool_615){
-                feedbackNode_615.push(wireUID_1415_);
-                feedbackNodeInitBool_615 = false;
-            }
-            wireUID_642_ = feedbackNode_615.shift();
-            /***** BEGIN CaseStructure ********/
-            String wireUID_2308_ = "A0";
-            bool wireUID_721_ = true;
-            bool wireUID_618_;
-            long wireUID_606_ = 2;
-            long wireUID_1038_ = 3;
-            String wireUID_2208_ = "A1";
-            bool wireUID_1239_;
-            bool wireUID_760_ = false;
-            long wireUID_1157_ = 2;
-            long wireUID_921_ = 3; //wires declarations
-            switch(wireUID_642_){
-                case false:
-                    
-                    wireUID_618_ = !wireUID_721_;
-                    digitalWrite(wireUID_1038_,wireUID_721_?HIGH:LOW);
-                    digitalWrite(wireUID_606_,wireUID_618_?HIGH:LOW);
-                    ArduinoLabVIEWcreatedUART_3->print(wireUID_2308_);
-                    
-                    
-                    
-                    
-                     /* C code */
-                    wireUID_446_ = wireUID_721_; /* output assgn. */
-                break;
-                case true:
-                    
-                    wireUID_1239_ = !wireUID_760_;
-                    digitalWrite(wireUID_921_,wireUID_760_?HIGH:LOW);
-                    digitalWrite(wireUID_1157_,wireUID_1239_?HIGH:LOW);
-                    ArduinoLabVIEWcreatedUART_3->print(wireUID_2208_);
-                    
-                    
-                    
-                    
-                     /* C code */
-                    wireUID_446_ = wireUID_760_; /* output assgn. */
-                break;
-            }
-            /****** END CaseStructure ********/
-            /* Don't know how to translate class "Generic ->GObject ->Node ->":  */
-            
-            feedbackNode_615.push(wireUID_446_);
-            
-             /* C code*/
-         /* output assignements */
+        double wireUID_1192_;
+        String wireUID_1322_ = shiftRegister_1326.shift();
+        String wireUID_1295_;
+        double wireUID_1118_ = shiftRegister_1173.shift();
+        double wireUID_1150_ = 4.200000;
+        bool wireUID_1116_;
+        wireUID_1116_ = (wireUID_1118_ >= wireUID_1150_);
+        /***** BEGIN CaseStructure ********/
+        String wireUID_1401_ = "Hello world.";
+        double wireUID_1266_ = 0.000000;
+        String wireUID_1313_ = wireUID_1322_;
+        double wireUID_1210_ = wireUID_1118_; //wires declarations
+        switch(wireUID_1116_){
+            case false:
+                
+                
+                 /* C code */
+                wireUID_1192_ = wireUID_1266_;
+                wireUID_1295_ = wireUID_1401_; /* output assgn. */
+            break;
+            case true:
+                
+                
+                 /* C code */
+                wireUID_1192_ = wireUID_1210_;
+                wireUID_1295_ = wireUID_1313_; /* output assgn. */
+            break;
         }
-        /***** END ForLoop ******/
-        /**** BEGIN ForLoop *****/
-        CircularBuffer<bool,2> feedbackNode_1321;
-        bool feedbackNodeInitBool_1321 = true;
-        
-        
-        for (int LVHardcodedCnt = 0;  LVHardcodedCnt < wireUID_1394_; LVHardcodedCnt++){
-            bool wireUID_1336_ = true;
-            bool wireUID_1339_;
-            bool wireUID_1342_;
-            long wireUID_1349_ = 1000;
-            if (feedbackNodeInitBool_1321){
-                feedbackNode_1321.push(wireUID_1336_);
-                feedbackNodeInitBool_1321 = false;
-            }
-            wireUID_1339_ = feedbackNode_1321.shift();
-            delay(wireUID_1349_); /* by Translator.vi, VI Path: C:\Users\lubo\Documents\LabView\LabVIEW-Universal-Transcriptor\Test\Examples For Parsing\delay.vi */
-            /***** BEGIN CaseStructure ********/
-            String wireUID_3039_ = "B0";
-            bool wireUID_1121_ = true;
-            long wireUID_1140_ = 2;
-            long wireUID_1156_ = 3;
-            String wireUID_2896_ = "B1";
-            bool wireUID_1298_ = false;
-            long wireUID_1308_ = 2;
-            long wireUID_1312_ = 3; //wires declarations
-            switch(wireUID_1339_){
-                case false:
-                    
-                    digitalWrite(wireUID_1156_,wireUID_1121_?HIGH:LOW);
-                    digitalWrite(wireUID_1140_,wireUID_1121_?HIGH:LOW);
-                    ArduinoLabVIEWcreatedUART_3->print(wireUID_3039_);
-                    
-                    
-                    
-                     /* C code */
-                    wireUID_1342_ = wireUID_1121_; /* output assgn. */
-                break;
-                case true:
-                    
-                    digitalWrite(wireUID_1312_,wireUID_1298_?HIGH:LOW);
-                    digitalWrite(wireUID_1308_,wireUID_1298_?HIGH:LOW);
-                    ArduinoLabVIEWcreatedUART_3->print(wireUID_2896_);
-                    
-                    
-                    
-                     /* C code */
-                    wireUID_1342_ = wireUID_1298_; /* output assgn. */
-                break;
-            }
-            /****** END CaseStructure ********/
-            /* Don't know how to translate class "Generic ->GObject ->Node ->":  */
-            feedbackNode_1321.push(wireUID_1342_);
-            
-            
-             /* C code*/
-         /* output assignements */
-        }
-        /***** END ForLoop ******/
+        /****** END CaseStructure ********/
         
          /* C code */
      /* output assgn. */
-    }while(!wireUID_1608_); //negation because in LabVIEW it is when stop (oposite)
+    }while(!wireUID_1433_); //negation because in LabVIEW it is when stop (oposite)
     /****** END WhileLoop */
-    
-    
-    
-    
-    
-    
     
 }
 
 void setup(){}
 void loop(){
-    Blink_5();
+    Example_Shift_Register_Loop_1(4.2, "hello");
 }
 
