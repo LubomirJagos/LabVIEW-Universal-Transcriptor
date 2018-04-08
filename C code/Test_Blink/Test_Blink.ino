2f4200c6490dfc1f0aeb21e01850290b3eec00f9
Lubomir Jagos <lubomir.jagos@gmail.com>
2018-04-08 09:24:14 +0200

    Added iterators into loops, changed looking for Translator.vi, now looking for something with Translator in name.
    I put there generated iterator for WhileLoop and ForLoop, theirs declarations are created in step 3. Wires are also in step 1. and 2. able assign value from it, this is done by checking if wire is connected to Terminal, then if its owner is Diagram and then if this Diagram owner is Loop (so I now that at this point that wire is connected to iterator terminal).
    
    Because in one project there cannot be two VI with same name, I changed looking for Translator.vi so now its looking for file which has in name Translator word.
    
    Also added folder with translator for my "System API", for now I like to call it this way :D

--- C code/Test_Blink/Test_Blink.ino
+++ C code/Test_Blink/Test_Blink.ino
@@ -2,81 +2,27 @@
 #include <vector>
 #include <ArduinoBasicFunctions.h>
 #include <CircularBuffer.h>
-#include <ArduinoBasicFunctions.h>
 using namespace std;
+typedef struct{long Person_Age;
+String Person_name;
+};
 
 
-void Blink_4_experimental(){
-    bool wireUID_740_ = true;
-    long wireUID_705_ = 3;
-    long wireUID_652_ = 2;
-    pinMode(wireUID_652_,wireUID_740_?OUTPUT:INPUT);
-    pinMode(wireUID_705_,wireUID_740_?OUTPUT:INPUT);
-    /***** BEGIN WhileLoop ********/
-    CircularBuffer<bool,2> feedbackNode_615;
-    bool feedbackNodeInitBool_615 = true;
-    
-    bool wireUID_430_ = false; //LoopEndRef.
-    do{
-     //shift register init
-        bool wireUID_761_ = false;
-        bool wireUID_642_;
-        bool wireUID_446_;
-        long wireUID_426_ = 200;
-        delay(wireUID_426_); /* by Translator.vi, VI Path: C:\Users\lubo\Documents\LabView\LabVIEW-Universal-Transcriptor\Test\Examples For Parsing\delay.vi */
-        if (feedbackNodeInitBool_615){
-            feedbackNode_615.push(wireUID_761_);
-            feedbackNodeInitBool_615 = false;
-        }
-        wireUID_642_ = feedbackNode_615.shift();
-        /***** BEGIN CaseStructure ********/
-        bool wireUID_721_ = true;
-        bool wireUID_618_;
-        long wireUID_606_ = 2;
-        long wireUID_1038_ = 3;
-        bool wireUID_1239_;
-        bool wireUID_760_ = false;
-        long wireUID_1157_ = 2;
-        long wireUID_921_ = 3; //wires declarations
-        switch(wireUID_642_){
-            case false:
-                
-                wireUID_618_ = !wireUID_721_;
-                digitalWrite(wireUID_1038_,wireUID_721_?HIGH:LOW);
-                digitalWrite(wireUID_606_,wireUID_618_?HIGH:LOW);
-                
-                
-                 /* C code */
-                wireUID_446_ = wireUID_721_; /* output assgn. */
-            break;
-            case true:
-                
-                wireUID_1239_ = !wireUID_760_;
-                digitalWrite(wireUID_921_,wireUID_760_?HIGH:LOW);
-                digitalWrite(wireUID_1157_,wireUID_1239_?HIGH:LOW);
-                
-                
-                 /* C code */
-                wireUID_446_ = wireUID_760_; /* output assgn. */
-            break;
-        }
-        /****** END CaseStructure ********/
-        /* Don't know how to translate class "Generic ->GObject ->Node ->":  */
-        
-        feedbackNode_615.push(wireUID_446_);
-        
-         //C code
-        
-         //output assgn.
-    }while(!wireUID_430_); //negation because in LabVIEW it is when stop (oposite)
-    /****** END WhileLoop */
-    
-    
+void Example_Cluster_2(){
+    struct{long Person_Age;
+    String Person_name;
+    } wireUID_924_;
+    String wireUID_843_ = "Mario Supersonic";
+    long wireUID_806_ = 42;
+    struct{long Person_Age;
+    String Person_name;
+    } wireUID_760_ = {0,};
+    wireUID_924_ = {.Person_Age = wireUID_806_,.Person_name = wireUID_843_}; //Bundler gen.
     
 }
 
 void setup(){}
 void loop(){
-    Blink_4_experimental();
+    Example_Cluster_2();
 }
 
--- C code/Test_Vector/Test_Vector.ino
+++ C code/Test_Vector/Test_Vector.ino
@@ -1,66 +1,38 @@
 #include <StandardCplusplus.h>
 #include <vector>
-#include "ArduinoBasicFunctions.h"
+#include <ArduinoBasicFunctions.h>
+#include <CircularBuffer.h>
 using namespace std;
-struct UserDefined_StructType{String LuboString;
-int NumInsideUserCluter;
-double Double_insideCluster;
-};
 
 
-
-vector<bool> Blink_3_simple_and_correct_one(){
-    UserDefined_StructType wireUID_813_ = {"LuboJe King",0,0.000000};
-    vector<bool> wireUID_770_;
-    vector<bool> wireUID_675_ = {true,true,false};
-    /***** BEGIN WhileLoop ********/
-    bool wireUID_529_; //LoopEndRef.Wire
-    do{
-        UserDefined_StructType wireUID_682_ = wireUID_813_;
-        UserDefined_StructType wireUID_634_;
-        vector<bool> wireUID_729_ = wireUID_675_;
-        vector<bool> wireUID_701_;
-        int wireUID_426_ = 42;
-        double wireUID_569_ = 7.000000;
-        int wireUID_540_;
-        Delay(wireUID_426_); /* by Translator.vi, VI Path: C:\Users\lubo\Documents\LabView\LabVIEW-Universal-Transcriptor\Test\Examples For Parsing\Delay.vi */
-        wireUID_529_ = wireUID_569_ > wireUID_540_;
-        /***** BEGIN CaseStructure ********/
-        
-        UserDefined_StructType wireUID_828_ = wireUID_682_;
-        vector<bool> wireUID_820_ = wireUID_729_;
-        int wireUID_575_ = -42;
-        
-        UserDefined_StructType wireUID_670_ = wireUID_682_;
-        vector<bool> wireUID_720_ = wireUID_729_;
-        int wireUID_953_ = -99; //wires declarations
-        switch(wireUID_529_){
-            case false:
-                
-                Delay(wireUID_575_); /* by Translator.vi, VI Path: C:\Users\lubo\Documents\LabView\LabVIEW-Universal-Transcriptor\Test\Examples For Parsing\Delay.vi */ /* C code */
-                wireUID_701_ = wireUID_820_;
-                wireUID_634_ = wireUID_828_; /* output assgn. */
-            break;
-            case true:
-                
-                Delay(wireUID_953_); /* by Translator.vi, VI Path: C:\Users\lubo\Documents\LabView\LabVIEW-Universal-Transcriptor\Test\Examples For Parsing\Delay.vi */
-                Toggle_LED(); /* C code */
-                wireUID_701_ = wireUID_720_;
-                wireUID_634_ = wireUID_670_; /* output assgn. */
-            break;
-        }
-        /****** END CaseStructure /* C code */
-        wireUID_770_ = wireUID_701_;
-//         = wireUID_634_; /* output assgn. */
-    }while(wireUID_529_);
-    /****** END WhileLoop */
-    vector<bool> BoolArrayIndicator1 = wireUID_770_;
+void Example_Test_4(){
+    long wireUID_287_ = 0;
+    /***** BEGIN CaseStructure ********/
+    vector<String> wireUID_393_;
+    String wireUID_511_ = "Hello world.";
+    vector<String> wireUID_370_ = {"A","B","C","D","E"};
+     //wires declarations
+    switch(wireUID_287_){
+        case  0:
+            
+            wireUID_370_.push_back(wireUID_511_);
+            wireUID_393_ = wireUID_370_;
+             /* C code */
+         /* output assgn. */
+        break;
+        case  1 :
+            
+            
+             /* C code */
+         /* output assgn. */
+        break;
+    }
+    /****** END CaseStructure ********/
     
-    return BoolArrayIndicator1;
 }
 
 void setup(){}
 void loop(){
-    Blink_3_simple_and_correct_one();
+    Example_Test_4();
 }
 
