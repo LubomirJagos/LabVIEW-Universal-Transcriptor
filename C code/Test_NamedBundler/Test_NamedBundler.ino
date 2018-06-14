d68c610c66ca59134b6360555ee52c76d6be1324
Lubomir Jagos <lubomir.jagos@gmail.com>
2018-06-14 21:03:52 +0200

    Corrected named unbundler tranbslation using cluster items datatypes and pointer arithmetic, removed whjitespaces from struct dfinition name (it was missing for cluster in Get Variant Data Type.vi).
    I repaired cluster unbundler translation, nbow it's using ipointer arithmetic, so named unbundler is translated as (we suppose that wireUID_42 is String and third item after int and String):
        wireUID_42 = *(String*) ((void*) &wireUID_42 + sizeof(int) + sizeof(String));
    
    Still there are duplications in cluster definitions, because if there are two cluster with same datatypes but different names, transcriptor generates for each typedefinition, but typedef name is generated from datatypes, so there are two typedefs but with different items names, these can be easily corrected remoivng typedef as text using simple string functions.

--- C code/Test_NamedBundler/Test_NamedBundler.ino
+++ C code/Test_NamedBundler/Test_NamedBundler.ino
@@ -1,51 +1,55 @@
-#include <StandardCplusplus.h>
+#include <ArduinoSTL.h>
 #include <vector>
 #include <ArduinoBasicFunctions.h>
 using namespace std;
-
-typedef struct UserDefined_StructType{String LuboString;
-int NumInsideUserCluter;
-double Double_insideCluster;
+typedef struct UserDefined_StructType{
+    String LuboString;
+    long NumInsideUserCluter;
+    double Double_insideCluster;
 };
 
 
 void Example_NamedBundler_1(){
-    int wireUID_264_ = 0;
+    long wireUID_264_ = 0;
     /***** BEGIN CaseStructure ********/
-    int wireUID_1137_ = 0;
-    String wireUID_1112_;
-    UserDefined_StructType wireUID_450_A = {"LuboJe King",0,0.000000};
-    int wireUID_1187_ = 0;
-    String wireUID_1162_;
-    UserDefined_StructType wireUID_273_C;
-    double wireUID_515_ = 4.200000;
-    int wireUID_491_ = 42;
-    String wireUID_467_ = "4.2.0";
-    UserDefined_StructType wireUID_416_B = {"LuboJe King",0,0.000000};
-    int wireUID_1087_;
-    String wireUID_1058_;
-    UserDefined_StructType wireUID_950_D = {"LuboJe King",0,0.000000}; //wires declarations
     switch(wireUID_264_){
-        case  0 :
+        case 3:
+        {
+            long wireUID_1087_;
+            String wireUID_1058_;
+            UserDefined_StructType wireUID_950_D = {"LuboJe King",0,0.000000};
+            wireUID_1058_ = *(String*) ((void*) &wireUID_950_D + 0);
+            wireUID_1087_ = *(long*) ((void*) &wireUID_950_D + sizeof(String));
             
              /* C code */
          /* output assgn. */
+        }
         break;
-        case  2 :
-            
-            wireUID_273_C = {.LuboString = wireUID_467_,.NumInsideUserCluter = wireUID_491_,.Double_insideCluster = wireUID_515_}; //Bundler gen. /* C code */
-         /* output assgn. */
-        break;
-        case  3 :
-            
-            wireUID_1058_ = wireUID_950_D.LuboString; //unbundle gen.
-            wireUID_1087_ = wireUID_950_D.NumInsideUserCluter; //unbundle gen.
+        case 2:
+        {
+            long wireUID_1187_ = 0;
+            String wireUID_1162_ = "";
+            UserDefined_StructType wireUID_273_C;
+            double wireUID_515_ = 4.200000;
+            long wireUID_491_ = 42;
+            String wireUID_467_ = "4.2.0";
+            UserDefined_StructType wireUID_416_B = {"LuboJe King",0,0.000000};
+            wireUID_273_C = {.LuboString = wireUID_467_,.NumInsideUserCluter = wireUID_491_,.Double_insideCluster = wireUID_515_}; //Bundler gen.
              /* C code */
          /* output assgn. */
+        }
+        break;
+        default:
+        {
+            long wireUID_1137_ = 0;
+            String wireUID_1112_ = "";
+            UserDefined_StructType wireUID_450_A = {"LuboJe King",0,0.000000};
+             /* C code */
+         /* output assgn. */
+        }
         break;
     }
     /****** END CaseStructure ********/
-    
     
 }
 
